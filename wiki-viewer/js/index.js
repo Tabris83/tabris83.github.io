83670afa13662ac9ef5f5246bd572b1d955eb431
Ben Moore <furby0@gmail.com>
2016-05-24 08:48:02 +1000

    starting JS code for wiki viewer

--- wiki-viewer/js/index.js
+++ wiki-viewer/js/index.js
@@ -1,6 +1,11 @@
 // JavaScript Document
 //wiki viewer JS code
-$("#sub-btn").click(function()
+/*$("#sub-btn").click(function()
 {
 	$(".search").val("test");
+});*/
+var URL_base = "";
+$("#sub-btn").click(function(e) 
+{
+	    
 });
